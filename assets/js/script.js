ad79822134a69f3565e7c72aaf9f4563a0cc6a3e
mcelhatton <colinm@mobiledealerdata.com>
2022-03-18 09:53:50 -0500

    saving initial page setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,12 @@
-var box = document.querySelector(".box");
-box.setAttribute("data-state", "hidden")
 
-var container = document.querySelector(".container");
+var startButton = document.querySelector('#start');
 
-container.addEventListener("click", function(event) {
-  var element = event.target;
-
-  // TODO: Complete function
-  if (element.dataset.state == "hidden") {
-    element.setAttribute("data-state", "shown");
-    element.textContent = element.dataset.number;
-  } else {
-    element.setAttribute("data-state", "hidden");
-    element.textContent = "";
-  }
-  
+startButton.addEventListener('click', function() {
+ document.getElementById('buttonDiv').style.display = 'block';
+ document.getElementById('start').style.display = 'none';
+ document.getElementById('h3El').style.display = 'none';
 });
+
+function codeQuiz(questions) {
+  
+}
